80952ae9d037a586488fa7c30d1b147d9d08698f
Admin <rizbi.cse@gmail.com>
2026-02-26 13:32:10 +0600

    v5

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", async function () {
   try {
     const response = await fetch('data.json');
@@ -198,32 +197,3 @@ function initHeroSlider() {
 
   setInterval(showNextSlide, slideInterval);
 }
-=======
-document.addEventListener("DOMContentLoaded", function () {
-
-    const slides = document.querySelectorAll(".hero-slide");
-
-    let currentIndex = 0;
-    const slideInterval = 4000; // 4 seconds
-
-    function showNextSlide() {
-
-        // remove active from current slide
-        slides[currentIndex].classList.remove("active");
-
-        // move to next
-        currentIndex++;
-
-        // reset if at end
-        if (currentIndex >= slides.length) {
-            currentIndex = 0;
-        }
-
-        // add active to new slide
-        slides[currentIndex].classList.add("active");
-    }
-
-    setInterval(showNextSlide, slideInterval);
-
-});
->>>>>>> e500abaea233967e5c83dbedef8c10412a83a2da
